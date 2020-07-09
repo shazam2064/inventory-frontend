fb44b30930baa8d3c86a52dc0b6be7afe22b4de0
Gabriel <shazam2064>
2020-07-09 08:39:49 -0400

    i added an icon

--- src/app/icons.module.ts
+++ src/app/icons.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { Archive, Home, LogOut, MapPin, Move, Package, User } from 'angular-feather/icons';
+import { Archive, Home, LogOut, MapPin, Move, Package, User, Clipboard, Layers } from 'angular-feather/icons';
 
 
 const icons = {
-  // Clipboard,
+  Clipboard,
   User,
   LogOut,
   Home,
   Archive,
   MapPin,
   Move,
-  Package
+  Package,
+  Layers
 };
 
 @NgModule({
