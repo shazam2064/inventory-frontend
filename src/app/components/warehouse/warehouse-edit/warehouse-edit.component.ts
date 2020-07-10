b65d5ce6ee5853974c931e8888b20f7c48fc9ef8
Gabriel <shazam2064>
2020-07-10 11:14:23 -0400

    i made the header component look better and i did some stuff in the warehouse edit

--- src/app/components/warehouse/warehouse-edit/warehouse-edit.component.ts
+++ src/app/components/warehouse/warehouse-edit/warehouse-edit.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {InventoryService} from "../../../services/inventory.service";
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { InventoryService } from "../../../services/inventory.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-warehouse-edit',
@@ -10,42 +10,45 @@ import {InventoryService} from "../../../services/inventory.service";
 })
 export class WarehouseEditComponent implements OnInit {
 
+  updatedWarehouse: FormGroup;
   id = this.route.snapshot.params['id'];
   warehouseData: any = {};
-  Warehouse: any = [];
 
   constructor(
     private inventoryService: InventoryService,
     private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.inventoryService.getWarehouse(this.id).subscribe((data: {}) => {
       this.warehouseData = data;
     });
+    this.updatedWarehouse = new FormGroup({
+      'name' : new FormControl('',
+        [ Validators.required, Validators.minLength(4)]
+      )
+    });
+  }
+
+  get name() {
+    return this.updatedWarehouse.get('name');
   }
 
 
   deleteWarehouse(id) {
-    if (window.confirm('Are you sure, you want to delete?')){
+    if (window.confirm('Are you sure, you want to delete?')) {
       this.inventoryService.deleteWarehouse(id).subscribe(data => {
-        // this.getWarehouseList();
         this.router.navigate(['/warehouse']);
       })
     }
   }
 
-  getWarehouseList() {
-    return this.inventoryService.getWarehouses().subscribe((data: {}) => {
-      this.Warehouse = data;
-    })
-  }
-
 
   updateWarehouse() {
-    if(window.confirm('Are you sure, you want to update?')){
-      this.inventoryService.updateWarehouse(this.id, this.warehouseData).subscribe(data => {
+    if (window.confirm('Are you sure, you want to update?')) {
+      this.inventoryService.updateWarehouse(this.id, this.updatedWarehouse).subscribe(data => {
         this.router.navigate(['/warehouse']);
       })
     }
