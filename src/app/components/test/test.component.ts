c4f1566ec22ff33d2da081c1ca04d0ce9f9a91a7
Gabriel <shazam2064>
2020-08-08 13:33:01 -0400

    A new form to check if it's valid or invalid

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,68 +1,21 @@
-import { NgModule,
-  Component,
-  Pipe,
-  OnInit
-} from '@angular/core';
-import {
-  ReactiveFormsModule,
-  FormsModule,
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
-  langs: string[] = [
-    'English',
-    'French',
-    'German',
-  ];
-  myform: FormGroup;
-  firstName: FormControl;
-  lastName: FormControl;
-  email: FormControl;
-  password: FormControl;
-  language: FormControl;
+export class TestComponent  {
+  myForm = new FormGroup({
+    'firstName':new FormControl('',Validators.required)
 
-
-  ngOnInit() {
-    this.createFormControls();
-    this.createForm();
+  })
+  //getter method
+  get firstName(){
+     return this.myForm.get('firstName');
   }
 
-  createFormControls() {
-    this.firstName = new FormControl('', Validators.required);
-    this.lastName = new FormControl('', Validators.required);
-    this.email = new FormControl('', [
-      Validators.required,
-      Validators.pattern("[^ @]*@[^ @]*")
-    ]);
-    this.password = new FormControl('', [
-      Validators.required,
-      Validators.minLength(8)
-    ]);
-    this.language = new FormControl('');
-  }
-
-  createForm() {
-    this.myform = new FormGroup({
-      name: new FormGroup({
-        firstName: this.firstName,
-        lastName: this.lastName,
-      }),
-      email: this.email,
-      password: this.password,
-      language: this.language
-    });
-  }
 }
 
 
