cee2f1a68bfbed1a28e73c56e64c800285c6112f
Gabriel <shazam2064>
2020-08-08 13:03:13 -0400

    Tests was changed

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,246 +1,71 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, Validators } from "@angular/forms";
-//
-// @Component({
-//   selector: 'app-test',
-//   templateUrl: './test.component.html',
-//   styleUrls: ['./test.component.css']
-// })
-// export class TestComponent {
-//
-//   isSubmitted = false;
-//
-//   // City Names
-//   City: any = ['Florida', 'South Dakota', 'Tennessee', 'Michigan']
-//
-//   constructor(public fb: FormBuilder) { }
-//
-//   /*########### Form ###########*/
-//   registrationForm = this.fb.group({
-//     cityName: ['', [Validators.required]]
-//   })
-//
-//
-//   // Choose city using select dropdown
-//   changeCity(e) {
-//     console.log(e.value)
-//     this.cityName.setValue(e.target.value, {
-//       onlySelf: true
-//     })
-//   }
-//
-//   // Getter method to access formcontrols
-//   get cityName() {
-//     return this.registrationForm.get('cityName');
-//   }
-//
-//   /*########### Template Driven Form ###########*/
-//   onSubmit() {
-//     this.isSubmitted = true;
-//     if (!this.registrationForm.valid) {
-//       return false;
-//     } else {
-//       alert(JSON.stringify(this.registrationForm.value))
-//     }
-//
-//   }
-//
-// }
-
-
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { InventoryService } from "../../services/inventory.service";
-import { Router } from "@angular/router";
-import { throwError } from "rxjs";
+import { NgModule,
+  Component,
+  Pipe,
+  OnInit
+} from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
+  langs: string[] = [
+    'English',
+    'French',
+    'German',
+  ];
+  myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
-  newItem: FormGroup;
-  public warehouseList;
-  public unitList;
-  public groupList;
-  public locationList;
-  isSubmitted = false;
-
-  constructor(private inventoryService: InventoryService, private router: Router, private fb: FormBuilder) {
-  }
 
   ngOnInit() {
-    this.getWarehouseList();
-    this.getUnitList();
-    this.getGroupList();
-    this.getLocationList();
-    this.newItem = new FormGroup({
-      'name': new FormControl('',
-        [Validators.required, Validators.minLength(4)]
-      ),
-      'description': new FormControl('',
-        [Validators.required, Validators.minLength(10)]
-      ),
-      'brand': new FormControl('',
-        [Validators.required, Validators.minLength(2)]
-      ),
-      'unit': new FormControl('',
-        [Validators.required]
-      ),
-      'group': new FormControl('',
-        [Validators.required]
-      ),
-      'location': new FormControl('',
-        [Validators.required]
-      ),
-      'warehouse': new FormControl('',
-        [Validators.required]
-      ),
-      'min': new FormControl('',
-        [Validators.required]
-      ),
-      'max': new FormControl('',
-        [Validators.required]
-      ),
-      'reorderPoint': new FormControl('',
-        [Validators.required]
-      ),
-      'entryDate': new FormControl('',
-        [Validators.required]
-      ),
-      'departureDate': new FormControl('',
-        [Validators.required]
-      ),
-      'ultimateValue': new FormControl('',
-        [Validators.required]
-      )
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+    this.language = new FormControl('');
+  }
+
+  createForm() {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: this.firstName,
+        lastName: this.lastName,
+      }),
+      email: this.email,
+      password: this.password,
+      language: this.language
     });
   }
-
-  get name() {
-    return this.newItem.get('name');
-  }
-
-  get description() {
-    return this.newItem.get('description');
-  }
-
-  get brand() {
-    return this.newItem.get('brand');
-  }
-
-  get warehouse() {
-    return this.newItem.get('warehouse');
-  }
-
-  get group() {
-    return this.newItem.get('group');
-  }
-
-  get unit() {
-    return this.newItem.get('unit');
-  }
-
-  get location() {
-    return this.newItem.get('location');
-  }
-
-  get minV() {
-    return this.newItem.get('min');
-  }
-
-  get maxV() {
-    return this.newItem.get('max');
-  }
-
-  get reorderPoint() {
-    return this.newItem.get('reorderPoint');
-  }
-
-  get entryDate() {
-    return this.newItem.get('entryDate');
-  }
-
-  get departureDate() {
-    return this.newItem.get('departureDate');
-  }
-
-  get ultimateValue() {
-    return this.newItem.get('ultimateValue');
-  }
-
-  changeWarehouse(e) {
-    console.log('e value ' + e.value)
-    this.warehouse.setValue(e.target.value, {
-      onlySelf: true
-    })
-  }
-
-  getWarehouseList() {
-    return this.inventoryService.getWarehouses().subscribe(
-      data => {
-        this.warehouseList = data;
-        console.log(data);
-      }
-    )
-  }
-
-  getUnitList() {
-    this.inventoryService.getUnits().subscribe(
-      data => {
-        this.unitList = data;
-        console.log('unit: ' + data);
-      },
-      err => console.error(err),
-      () => console.log('units loaded')
-    );
-  }
-
-  getGroupList() {
-    this.inventoryService.getGroups().subscribe(
-      data => {
-        this.groupList = data;
-        console.log('group: ' + data);
-      },
-      err => console.error(err),
-      () => console.log('groups loaded')
-    );
-  }
-
-
-  getLocationList() {
-    this.inventoryService.getLocations().subscribe(
-      data => {
-        this.locationList = data;
-        console.log('location: ' + data);
-      },
-      err => console.error(err),
-      () => console.log('locations loaded')
-    );
-  }
-
-  submitItem() {
-    this.isSubmitted = true;
-    if (!this.newItem.valid) {
-      return false;
-    } else {
-      alert(JSON.stringify(this.newItem.value))
-    }
-  }
-
-  submitItem2() {
-    this.isSubmitted = true;
-    if (!this.newItem.valid) {
-      return false;
-    } else {
-      console.log("Your item has been created. Thank you!");
-      this.router.navigate(['item']);
-    }
-  }
-
-
 }
+
+
+
 
 
